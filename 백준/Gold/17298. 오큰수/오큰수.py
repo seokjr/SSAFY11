bdaa9a332747b4802ffbb07ab8311f19d0686911
seokjr <116258012+seokjr@users.noreply.github.com>
2024-02-07 13:51:15 +0900

    [Gold IV] Title: 오큰수, Time: 1096 ms, Memory: 155548 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.py
@@ -0,0 +1,11 @@
+N = int(input())
+A = list(map(int, input().split()))
+NGE= [-1]*N
+stack = [0] # 0번 인덱스
+
+for i in range(1, N):
+    # 오큰수 : A[i]의 오른쪽에 있으면서 A[i]보다 큰 수 중 가장 왼쪽 값 
+    while stack and A[stack[-1]] < A[i]:
+        NGE[stack.pop()] = A[i] # 해당 인덱스 칸은 A[i]
+    stack.append(i)
+print(*NGE)
