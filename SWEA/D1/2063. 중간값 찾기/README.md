435e3b2ebed96930645f2193da364cfc202cd7db
seokjr <116258012+seokjr@users.noreply.github.com>
2024-02-07 15:07:32 +0900

    [D1] Title: 중간값 찾기, Time: 102 ms, Memory: 41,928 KB -BaekjoonHub

---
+++ SWEA/D1/2063. 중간값 찾기/README.md
@@ -0,0 +1,15 @@
+# [D1] 중간값 찾기 - 2063 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV5QPsXKA2UDFAUq) 
+
+### 성능 요약
+
+메모리: 41,928 KB, 시간: 102 ms, 코드길이: 547 Bytes
+
+### 제출 일자
+
+2024-02-07 15:07
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D1/2063. 중간값 찾기/중간값 찾기.py
@@ -0,0 +1,15 @@
+# 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+# 아래 표준 입출력 예제 필요시 참고하세요.
+
+# 표준 입력 예제
+'''
+b, c = map(int, input().split())        정수형 변수 2개 입력 받는 예제 
+d = float(input())                      실수형 변수 1개 입력 받는 예제
+e, f, g = map(float, input().split())   실수형 변수 3개 입력 받는 예제
+h = input()                             문자열 변수 1개 입력 받는 예제
+'''
+a = int(input())                        
+lst = list(map(int, input().split()))
+lst.sort()
+answer = lst[a // 2]
+print(answer)
