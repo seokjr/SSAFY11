231afdd0cffd6a6ad0731cc24f4092ac1a6ef4b5
seokjr <116258012+seokjr@users.noreply.github.com>
2024-02-16 09:42:33 +0900

    [D3] Title: 농작물 수확하기, Time: 167 ms, Memory: 30,596 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -6,27 +6,52 @@ public class Solution {
 		int T = sc.nextInt();
 		for (int tc = 1; tc <= T; tc++) {
 			int N = sc.nextInt();
-			int[][] arr = new int[N][N];
-			for (int i = 0; i < N; i++) {
-				String t = sc.next();
-				for (int j = 0; j<t.length();j++) {
-					arr[i][j] = Character.getNumericValue(t.charAt(j));
+			int[][] board = new int[N][N];
+			for (int r = 0; r < N; r++) {
+				String line = sc.next();
+				for (int c = 0; c < N; c++) {
+					board[r][c] = line.charAt(c) - '0';
 				}
 			}
-			int start = (int) (N / 2);
-			int cnt = 0;
-			for (int i = 0; i < N; i++) {
-				if (i <= start) {
-					for (int j = start - i; j < N - start + i; j++) {
-						cnt += arr[i][j];
-					}
-				} else {
-					for (int j = i - start; j < N - i + start; j++) {
-						cnt += arr[i][j];
-					}
+			// 마름모 형태로 순회
+			int sum = 0;
+			int left = N/2;
+			int right = N/2;
+			
+			// 위쪽 삼각형
+			for (int r = 0; r < N/2; r++) {
+				for (int c = left; c <= right; c++) {
+					sum += board[r][c];
 				}
+				left--;
+				right++;
 			}
-			System.out.println("#" + tc + " " + cnt);
+			// 아래쪽 삼각형
+			for (int r = N/2; r<N;r++) {
+				for (int c = left; c <= right; c++) {
+					sum += board[r][c];
+				}
+				left++;
+				right--;
+			}
+			
+//			// 여긴 삼각형을 한 번에 
+//			for (int r = 0; r < N; r++) {
+//				int mid = N / 2;
+//				if (r < N / 2) {
+//					// 한 행 탐색 범위 증가
+//					for (int c = mid - r; c <= mid + r; c++) {
+//						sum += board[r][c];
+//					}
+//				} else {
+//					// 한 행 탐색 범위 감소
+//					for (int c = mid - (N - 1 - r); c <= mid + (N - 1 - r); c++) {
+//						sum += board[r][c];
+//					}
+//
+//				}
+//			}
+			System.out.println("#" + tc + " " + sum);
 		}
 	}
 }
