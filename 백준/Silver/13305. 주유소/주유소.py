8ac54fa03d60e6073dfbe94cefe995f84176ae71
seokjr <116258012+seokjr@users.noreply.github.com>
2024-02-22 08:49:24 +0900

    [Silver III] Title: 주유소, Time: 112 ms, Memory: 46788 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -3,23 +3,11 @@ liters = list(map(int, input().split()))
 cities = list(map(int, input().split()))
 
 ans = 0
-for i in range(N-1):
-    arr = []
-    # 최소값일 때는 다 사고
-    if cities[i] <= min(cities[i+1:]):
-        ans += sum(liters[i:]) * cities[i]
-        # 멈춘다.
-        break
-    # 최소값이 아닐 때는 최소값인 도시까지의 기름량 만큼만
-    else:
-        minval = min(cities[i+1:])
-        for m in range(i, N-1):
-            # 최솟값인 도시의 인덱스를 찾아 놓는다.
-            if cities[m] == minval:
-                t = m
-                break
-        # 현재부터 최소값인 도시까지의 기름량 만큼을 산다.
-        for k in range(i, m+1):
-            ans += liters[k] * cities[i]
-        # ans += liters[i:m+1] * cities[i]
+cur_price = cities[0]
+for i in range(N - 1):
+    # 이전 가격이 현재 가격보다 비싸면 갱신
+    if cur_price > cities[i]:
+        cur_price = cities[i]
+    # 그렇지 않다면 그냥 현재 기름이랑 liters 만큼이랑 곱한다.
+    ans += cur_price * liters[i]
 print(ans)
