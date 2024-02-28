7b86df467e6771edd7fdb5569249190fa9602023
seokjr <116258012+seokjr@users.noreply.github.com>
2024-02-28 14:04:49 +0900

    [Bronze I] Title: 색종이, Time: 1736 ms, Memory: 38304 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/10163. 색종이/색종이.py
+++ 백준/Bronze/10163. 색종이/색종이.py
@@ -5,9 +5,7 @@ cnt = [0] * (N+1)
 for m in range(1, N+1):
     x, y, width, height = map(int, input().split())
     for i in range(y, y+height):
-        for j in range(x, x+width):
-            # 들어오는 순서대로 넘버를 붙여서 바꾼다
-            arr[i][j] = m
+        arr[i][x:x+width] = [m] * width
 
 # 행을 돌면서 숫자 count 만큼을 더해준다.
 for m in range(1, N+1):
