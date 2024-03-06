5cb8c7d9cc990535703a11e3b3322486a62c9d08
ahddol <ahddol@naver.com>
2024-03-06 14:09:09 +0900

    사용자관리jsp추가

--- 1.강의들/backend/practice/back_db_hw_01_2_사용자/src/main/java/com/ssafy/hw/step2/servlet/MainServlet.java
+++ 1.강의들/backend/practice/back_db_hw_01_2_사용자/src/main/java/com/ssafy/hw/step2/servlet/MainServlet.java
@@ -2,13 +2,12 @@ package com.ssafy.hw.step2.servlet;
 
 import java.io.IOException;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-import com.ssafy.hw.step2.dto.User;
 
 //이 서블릿이 호출되기 위해서는 url 상에 http://server_ip:port/context_name/main 이 필요하다.
 @WebServlet("/main")
@@ -70,21 +69,19 @@ public class MainServlet extends HttpServlet{
 		String password = request.getParameter("password");
 		String name = request.getParameter("name");
 		String email = request.getParameter("email");
-//		// 문자열로 전달된 age는 숫자로 변환
+		// 문자열로 전달된 age는 숫자로 변환
 		int age = Integer.parseInt(request.getParameter("age"));
 		
-//		// 전달받은 parameter를 이용해서 User 객체를 생성한다. 
-		User user = new User(id, password, name, email, age);
+		// 전달받은 파라미터를 request에 담는다.
+		request.setAttribute("id", id);
+		request.setAttribute("password", password);
+		request.setAttribute("name", name);
+		request.setAttribute("email", email);
+		request.setAttribute("age", age);
 		
-//		// 화면에 출력할 데이터를 구성한다. 
-		StringBuilder output = new StringBuilder();
-		
-		output.append("<html><body>").append("<h1>사용자 정보</h1>").append(user.toString()).append("</body></html>");
-		
-		// response 객체를 통해서 생성한 html코드를 출력한다.
-		// 응답이 어떤 타입인지 설정
-		response.setContentType("text/html; charset=UTF-8");
-		response.getWriter().write(output.toString());
+		// reuqest에 담은 정보들을 결과 패이지에 넘겨준다.
+		RequestDispatcher disp = request.getRequestDispatcher("/regist_result.jsp");
+		disp.forward(request, response);
 		
 	}
 }
