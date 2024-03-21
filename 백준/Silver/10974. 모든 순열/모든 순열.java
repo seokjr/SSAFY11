7f47fabf6abb3a7913a8ab125bbb33bdd637b379
seokjr <116258012+seokjr@users.noreply.github.com>
2024-03-21 14:23:05 +0900

    [Silver III] Title: 모든 순열, Time: 2680 ms, Memory: 79692 KB -BaekjoonHub

--- 백준/Silver/10974. 모든 순열/모든 순열.java
+++ 백준/Silver/10974. 모든 순열/모든 순열.java
@@ -1,39 +1,32 @@
-import java.util.Scanner;
+import java.io.*;
+import java.util.*;
 
 public class Main {
+	static int[] select;
+	static boolean[] visit;
 	static int N;
-	static int[] arr;
-	static int[] result;
-	static boolean[] visited;
-
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
-		arr = new int[N];
-		visited = new boolean[N];
-		result = new int[N];
-		int tmp = 0;
-		for (int i = 1; i <= N; i++) {
-			arr[tmp++] = i;
-		}
-		perm(0);
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		N = Integer.parseInt(br.readLine());
+		select = new int[N];
+		visit = new boolean[N + 1];
+		permutation(0);
 	}
-
-	static void perm(int idx) {
-		if (idx >= N) {
-			for (int i : result) {
+	
+	static void permutation(int idx) {
+		if(idx == N) {
+			for(int i : select) {
 				System.out.print(i + " ");
 			}
 			System.out.println();
 			return;
 		}
-		for (int i = 0; i<N; i++) {
-			if (visited[i]) continue;
-			visited[i] = true;
-			result[idx] = arr[i];
-			perm(idx+1);
-			visited[i] = false;
+		for(int i = 1; i <= N; i++) {
+			if(visit[i]) continue;
+			visit[i] = true;
+			select[idx] = i;
+			permutation(idx + 1);
+			visit[i] = false;
 		}
 	}
-
 }
